107e6c19292a50b95308fb3df83d2c17d9244913
YRlp98 <YRlp98@users.noreply.github.com>
2021-12-16 15:41:45 +0000

    deploy: 8c6812f5c16f61ccae9ce165cede0c45178aeff3

---
+++ _nuxt/static/1639669304/en/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpersonal_portfolio_website\u002F_nuxt\u002Fstatic\u002F1639669304",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen",config:{_app:{basePath:"\u002Fpersonal_portfolio_website\u002F",assetsPath:"\u002Fpersonal_portfolio_website\u002F_nuxt\u002F",cdnURL:a}}}}(null));
