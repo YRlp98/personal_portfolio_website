dba5f19bd3ded07a48093f3d6f5a68d1bed17622
YRlp98 <YRlp98@users.noreply.github.com>
2022-01-26 14:39:34 +0000

    deploy: 33ec39fcc5bdc3e9df6aba8388e9b7676893b8f3

--- _nuxt/static/1643056148/https:/virgool.io/@YRlp98/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643056148",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fhttps:\u002F\u002Fvirgool.io\u002F@YRlp98"));
